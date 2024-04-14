7a16a6f93c5a6630afa0b35b96a45c3e94428a51
djdw04 <144089704+djdw04@users.noreply.github.com>
2024-04-14 15:54:03 +0800

    Done

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,40 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> listen = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> names){
+        int cnt = 0;
+        for(String name:names){
+            name = name.toLowerCase();
+            int num = 0;
+            for(String newName:listen){
+                if(name.equals(newName)){
+                    num = 1;
+                    break;
+                }
+            }
+            if(num == 0){
+                cnt++;
+                listen.add(name);
+            }
+        }
+        return cnt;
+    }
 }
+
